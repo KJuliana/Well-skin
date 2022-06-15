72fd03bf891155a0ca87d0f7b2f974e33ad33b4e
kjuliana <korjuliana@gmail.com>
2022-06-15 20:04:46 +0300

    Обновление иконки

--- components/page.php
+++ components/page.php
@@ -10,7 +10,7 @@ function render_page(string $title, string $header_html, string $footer_html, st
     <title>$title – Well Skin</title>
     <link rel='stylesheet' type='text/css' href='style.css'/>
     <meta name='description' content='Анализ и сраванение составов косметики'>
-    <link href='img/cream.ico' rel='shortcut icon' type='image/x-icon'>
+    <link href='./img/favicon-white.png' rel='shortcut icon' type='image/png'>
 </head>
 
 <body>
