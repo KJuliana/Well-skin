f79783cf36af25f56174b2750a38c4df05b9e548
kjuliana <korjuliana@gmail.com>
2022-07-06 15:00:58 +0300

    Добавление веб-сайта в поисковую систему яндекс

--- @components/page.php
+++ @components/page.php
@@ -1,4 +1,6 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/@components/metrika.php');
+
 function render_page(string $title, string $header_html, string $footer_html, string $body_html, string $page_class = ""): string {
     $css_modify_time = filemtime($_SERVER['DOCUMENT_ROOT'] . '/style.css'); // для сброса кеша при изменении стилей
     return "
@@ -7,13 +9,14 @@ function render_page(string $title, string $header_html, string $footer_html, st
 
 <head>
     <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no,'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, minimum-scale=1.0'>
+    <meta name='yandex-verification' content='b1bb7a24f3f30d0c'>
     <title>$title – Well Skin</title>
     <link rel='stylesheet' type='text/css' href='/style.css?$css_modify_time'/>
     <meta name='description' content='Анализ и сраванение составов косметики'>
     <link href='/img/favicon-white.png' rel='shortcut icon' type='image/png'>
+    " . render_metrika_snippet() . "
 </head>
-
 <body>
 
 <div class='page $page_class'>
