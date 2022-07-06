7ab54d4064820f8672b883b1f41ca24eb8818a0a
kjuliana <korjuliana@gmail.com>
2022-07-06 18:59:36 +0300

    Оформление цвета линии для ссылок

--- about/index.php
+++ about/index.php
@@ -11,6 +11,17 @@ $title = "О проекте";
 $body = "
 <section class='intro'>
     <p class='intro__text'>Данный проект реализован с целью повышения доступности и популяризации научного подхода при выборе косметических средств.</p>
-</section>";
+</section>
+<section class='intro'>
+    <p class='intro__text'>Индексы </p>
+</section>
+<section class='intro'>
+    <p class='intro__text'> Функции, которые отображаются на странице ингредиента, взяты из базы данных CosIng </p>
+</section>
+<section class='intro'>
+    <p class='intro__text'> Таблица с рисками</p>
+</section>
+";
+
 
 echo render_page($title, $header, $footer, $body);
